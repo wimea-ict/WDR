535cec1daa1ccf7f3d5a4ba0c844850bb5d35474
NATHERN KUNYA <32688406+NathernKunya@users.noreply.github.com>
2023-06-15 13:12:20 +0300

    added files

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+/*
+Template Name: Groci - Organic Food & Grocery Market Template
+Author: Askbootstrap
+Author URI: https://themeforest.net/user/askbootstrap
+Version: 1.1
+*/
+$(document).ready(function() {
+
+    $(".select2").select2();
+
+});
